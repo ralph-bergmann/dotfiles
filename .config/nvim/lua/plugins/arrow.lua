2c1f6a3c5374b02fd67b569d334a067cf9d7af51
Ralph Bergmann <bergmann@karlmax-berlin.com>
2024-04-02 12:09:33 +0200

    small nvim plugin updates

--- .config/nvim/lua/plugins/arrow.lua
+++ .config/nvim/lua/plugins/arrow.lua
@@ -1,5 +1,9 @@
 -- Bookmark your files, separated by project, and quickly navigate through them.
 return {
    "otavioschwanck/arrow.nvim",
-   opts = {}
+   opts = {
+      show_icons = true,
+      separate_save_and_remove = true,
+      leader_key = '.'
+   }
 }
--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,13 +2,7 @@ return {
    "folke/flash.nvim",
    event = "VeryLazy",
    ---@type Flash.Config
-   opts = {
-      modes = {
-         char = {
-            autohide = true,
-         }
-      }
-   },
+   opts = {},
    -- stylua: ignore
    keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
