4b98963f773bc43f153412efae19309b34e70c86
Ralph Bergmann <bergmann@karlmax-berlin.com>
2024-04-05 18:22:03 +0200

    nvim: fix autosave setup

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -2,20 +2,17 @@ return {
    "okuuva/auto-save.nvim",
    cmd = "ASToggle",
    event = { "InsertLeave", "TextChanged" },
-   config = function()
-      require("auto-save").setup({
-         execution_message = { enabled = false },
-         trigger_events = { "InsertLeave", "TextChanged" },
-         condition = function(buf)
-            local fn = vim.fn
-            local utils = require("auto-save.utils.data")
+   opts = {
+      execution_message = { enabled = false },
+      condition = function(buf)
+         local fn = vim.fn
+         local utils = require("auto-save.utils.data")
 
-            -- don't save for `oil` file types
-            if utils.not_in(fn.getbufvar(buf, "&filetype"), { 'oil' }) then
-               return true
-            end
-            return false
+         -- don't save for `oil` file types
+         if utils.not_in(fn.getbufvar(buf, "&filetype"), { 'oil' }) then
+            return true
          end
-      })
-   end,
+         return false
+      end
+   },
 }
