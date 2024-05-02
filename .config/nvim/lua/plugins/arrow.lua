00efaa60ca99938a2bfdc106396026ecf33ebfa5
Ralph Bergmann <bergmann@karlmax-berlin.com>
2024-05-02 18:30:35 +0200

    nvim: remove unused plugins

--- .config/nvim/lua/plugins/arrow.lua
+++
@@ -1,9 +0,0 @@
--- Bookmark your files, separated by project, and quickly navigate through them.
-return {
-   "otavioschwanck/arrow.nvim",
-   opts = {
-      show_icons = true,
-      separate_save_and_remove = true,
-      leader_key = '.'
-   }
-}
