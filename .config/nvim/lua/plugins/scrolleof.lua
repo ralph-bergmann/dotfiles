2a68f7c2614611f6caf28db7223f572b464d13eb
Ralph Bergmann <bergmann@karlmax-berlin.com>
2024-05-12 12:43:18 +0200

    nvim: enable scrolleof in insert mode

--- .config/nvim/lua/plugins/scrolleof.lua
+++ .config/nvim/lua/plugins/scrolleof.lua
@@ -1,5 +1,10 @@
 return {
    'Aasim-A/scrollEOF.nvim',
    event = { 'CursorMoved', 'WinScrolled' },
-   opts = {},
+   config = function()
+      require('scrollEOF').setup({
+         -- Whether or not scrollEOF should be enabled in insert mode
+         insert_mode = true,
+      })
+   end
 }
