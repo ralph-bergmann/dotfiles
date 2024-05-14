237044e9826c8deb0c451f06742dd7fafec73527
Ralph Bergmann <bergmann@karlmax-berlin.com>
2024-05-14 10:29:35 +0200

    nvimg; update flash config

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,10 +2,16 @@ return {
    "folke/flash.nvim",
    event = "VeryLazy",
    ---@type Flash.Config
-   opts = {},
    -- stylua: ignore
+   opts = {
+      modes = {
+         search = {
+            enabled = true,
+         }
+      }
+   },
    keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
    },
 }
