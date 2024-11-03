40f5c41af2eba01b229c5b49ed56f366c3cade13
Ralph Bergmann <ralph@dasralph.de>
2024-11-03 14:34:39 +0100

    nvim: fix nvim config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,84 +1,84 @@
 local function get_short_cwd()
-   return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
 end
 
 
 local neotree_extension = {
-   sections = {
-      lualine_a = { get_short_cwd },
-      lualine_y = { "branch" },
-   },
-   filetypes = { 'neo-tree' },
+  sections = {
+    lualine_a = { get_short_cwd },
+    lualine_y = { "branch" },
+  },
+  filetypes = { 'neo-tree' },
 }
 
 return {
-   "nvim-lualine/lualine.nvim",
-   config = function()
-      require("lualine").setup({
-         options = {
-            theme = "tokyonight",
-            component_separators = "|",
-            section_separators = { left = "", right = "" },
-            disabled_filetypes = {
-               "oil",
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "tokyonight",
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          "oil",
+        },
+        globalstatus = false,
+      },
+      sections = {
+        lualine_a = {
+          {
+            "filename",
+            file_status = true,
+            newfile_status = true,
+            path = 1,
+            symbols = {
+              modified = "󱇨",
+              readonly = "󱀰",
+              unnamed = "󱀷",
+              newfile = "󰻭",
             },
-            globalstatus = false,
-         },
-         sections = {
-            lualine_a = {
-               {
-                  "filename",
-                  file_status = true,
-                  newfile_status = true,
-                  path = 1,
-                  symbols = {
-                     modified = "󱇨",
-                     readonly = "󱀰",
-                     unnamed = "󱀷",
-                     newfile = "󰻭",
-                  },
-               },
+          },
+        },
+        lualine_b = { 'diagnostics', "location" },
+        lualine_c = {
+          {
+            "navic",
+            color_correction = nil,
+            navic_opts = nil,
+          },
+        },
+        lualine_x = { "diff" },
+        lualine_y = { "branch" },
+        lualine_z = {},
+      },
+      inactive_sections = {
+        lualine_a = {
+          {
+            "filename",
+            file_status = true,
+            newfile_status = true,
+            path = 1,
+            symbols = {
+              modified = "󱇨",
+              readonly = "󱀰",
+              unnamed = "󱀷",
+              newfile = "󰻭",
             },
-            lualine_b = { 'diagnostics', "location" },
-            lualine_c = {
-               {
-                  "navic",
-                  color_correction = nil,
-                  navic_opts = nil,
-               },
-            },
-            lualine_x = { "diff" },
-            lualine_y = { "branch" },
-            lualine_z = {},
-         },
-         inactive_sections = {
-            lualine_a = {
-               {
-                  "filename",
-                  file_status = true,
-                  newfile_status = true,
-                  path = 1,
-                  symbols = {
-                     modified = "󱇨",
-                     readonly = "󱀰",
-                     unnamed = "󱀷",
-                     newfile = "󰻭",
-                  },
-               },
-            },
-            lualine_b = { 'diagnostics', "location" },
-            lualine_c = {
-               {
-                  "navic",
-                  color_correction = nil,
-                  navic_opts = nil,
-               },
-            },
-            lualine_x = { "diff" },
-            lualine_y = { "branch" },
-            lualine_z = {},
-         },
-         extensions = { neotree_extension, "symbols-outline" },
-      })
-   end,
+          },
+        },
+        lualine_b = { 'diagnostics', "location" },
+        lualine_c = {
+          {
+            "navic",
+            color_correction = nil,
+            navic_opts = nil,
+          },
+        },
+        lualine_x = { "diff" },
+        lualine_y = { "branch" },
+        lualine_z = {},
+      },
+      extensions = { neotree_extension, "symbols-outline" },
+    })
+  end,
 }
--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,19 +1,20 @@
 return {
-   "folke/tokyonight.nvim",
-   lazy = false,
-   priority = 1000,
-   opts = {
-      style = "night",
-      on_highlights = function(highlights, colors)
-         highlights.WinSeparator = { fg = colors.blue7 }
-         highlights.CursorLineNr = { fg = colors.green }
-         highlights.IlluminatedWordText = { bg = "#4F69A1", fg = "#E6EAFF" }
-         highlights.IlluminatedWordRead = { bg = "#5B773D", fg = "#E6EAFF" }
-         highlights.IlluminatedWordWrite = { bg = "#A04D5C", fg = "#E6EAFF" }
-      end,
-   },
-   config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd.colorscheme("tokyonight")
-   end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night",
+    on_highlights = function(highlights, colors)
+      highlights.WinSeparator = { fg = colors.blue7 }
+      highlights.CursorLineNr = { fg = colors.green }
+      highlights.IlluminatedWordText = { bg = "#4F69A1", fg = "#E6EAFF" }
+      highlights.IlluminatedWordRead = { bg = "#5B773D", fg = "#E6EAFF" }
+      highlights.IlluminatedWordWrite = { bg = "#A04D5C", fg = "#E6EAFF" }
+      -- highlights.guibg = colors.red
+    end,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd.colorscheme("tokyonight")
+  end,
 }
