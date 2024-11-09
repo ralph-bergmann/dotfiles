77e1d5f938098a0775059d21f3cde6eaf3a66664
Ralph Bergmann <ralph@dasralph.de>
2024-11-09 15:22:43 +0100

    nvim: add rust support

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,29 @@
 return {
-   {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      config = function()
-         local config = require("nvim-treesitter.configs")
-         config.setup({
-            ensure_installed = {
-               "bash",
-               "dart",
-               "json",
-               "lua",
-               "toml",
-            },
-            -- auto_install = true,
-            highlight = { enable = true },
-            indent = {
-               enable = true,
-               -- disable = { "dart" }
-            },
-         })
-      end,
-   },
-   {
-      -- Show code context (e.g. the class name or function name) at the top of the buffer
-      "nvim-treesitter/nvim-treesitter-context",
-   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {
+          "bash",
+          "dart",
+          "json",
+          "lua",
+          "rust",
+          "toml",
+        },
+        auto_install = false,
+        highlight = { enable = true },
+        indent = {
+          enable = true,
+          -- disable = { "dart" }
+        },
+      })
+    end,
+  },
+  {
+    -- Show code context (e.g. the class name or function name) at the top of the buffer
+    "nvim-treesitter/nvim-treesitter-context",
+  },
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 3. `git clone --bare git@github.com:ralph-bergmann/dotfiles.git $HOME/.dotfiles`
 4. `dotfiles checkout`
 5. `dotfiles config --local status.showUntrackedFiles no`
-6. `brew install nvim bat eza ripgrep zoxide fzf tmux starship lazygit difftastic`
+6. `brew install nvim bat eza ripgrep zoxide fzf tmux starship lazygit difftastic rust-analyzer`
 7. `brew install --cask alacritty`
 
 If `fzf` completion isn't working run `$(brew --prefix)/opt/fzf/install`.
@@ -13,4 +13,7 @@ If `fzf` completion isn't working run `$(brew --prefix)/opt/fzf/install`.
 Install `tmux` `tpm`: `git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm`
 In a `tmux` session press `<leader> I` to install all plugins.
 
+Rename the original `rust-analyzer` so that the one needed for nvim can be taken from `brew`.
+
 see [Dotfiles: Best way to store in a bare git repository](https://www.atlassian.com/git/tutorials/dotfiles)
+
