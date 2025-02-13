46e903003643a3bd022a9ca555f938a27e3d25a3
Ralph Bergmann <ralph@dasralph.de>
2025-02-13 11:29:04 +0100

    update cofig

---
+++ .config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,16 @@
+return {
+  "rachartier/tiny-inline-diagnostic.nvim",
+  event = "VeryLazy", -- Or `LspAttach`
+  priority = 1000,    -- needs to be loaded in first
+  config = function()
+    require('tiny-inline-diagnostic').setup({
+      preset = "powerline",
+      show_all_diags_on_cursorline = true,
+      options = {
+        -- multilines = true,
+        use_icons_from_diagnostic = true,
+      },
+    })
+    vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+  end
+}
