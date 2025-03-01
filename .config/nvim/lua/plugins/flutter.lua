71a8541571f697a6d2dc7f428fb4e6689913fc59
Ralph Bergmann <ralph@dasralph.de>
2025-03-01 14:02:13 +0100

    nvim: enable fvm option for flutter plugin

--- .config/nvim/lua/plugins/flutter.lua
+++ .config/nvim/lua/plugins/flutter.lua
@@ -1,28 +1,29 @@
 return {
-   "akinsho/flutter-tools.nvim",
-   lazy = false,
-   dependencies = {
-      "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim",
-   },
-   config = function()
-      local lsp_zero = require("lsp-zero")
-      require("flutter-tools").setup({
-         -- widget_guides = {
-         --    enabled = true,
-         -- },
-         lsp = {
-            capabilities = lsp_zero.get_capabilities(),
-            color = {
-               enabled = true,
-               background = true,
-               virtual_text = false,
-            },
-            settings = {
-               lineLength = 100,
-               renameFilesWithClasses = 'always',
-            },
-         },
-      })
-   end,
+  "akinsho/flutter-tools.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+  },
+  config = function()
+    local lsp_zero = require("lsp-zero")
+    require("flutter-tools").setup({
+      fvm = true,
+      widget_guides = {
+        enabled = true,
+      },
+      lsp = {
+        capabilities = lsp_zero.get_capabilities(),
+        color = {
+          enabled = true,
+          background = true,
+          virtual_text = false,
+        },
+        settings = {
+          lineLength = 100,
+          renameFilesWithClasses = 'always',
+        },
+      },
+    })
+  end,
 }
